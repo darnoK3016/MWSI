3545cbe1b2ad8888a897d1bc4990e8a08d270b8d
darnoK3016 <44728558+darnoK3016@users.noreply.github.com>
2018-12-17 08:13:50 +0100

    Import project

---
+++ MWSI/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace MWSI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openCADRGFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cADRGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openCADRGFileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(699, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openCADRGFileToolStripMenuItem
+            // 
+            this.openCADRGFileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.openCADRGFileToolStripMenuItem.Name = "openCADRGFileToolStripMenuItem";
+            this.openCADRGFileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.openCADRGFileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cADRGToolStripMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // cADRGToolStripMenuItem
+            // 
+            this.cADRGToolStripMenuItem.Name = "cADRGToolStripMenuItem";
+            this.cADRGToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.cADRGToolStripMenuItem.Text = "CADRG";
+            this.cADRGToolStripMenuItem.Click += new System.EventHandler(this.cADRGToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(535, 437);
+            this.flowLayoutPanel1.TabIndex = 1;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 461);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openCADRGFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cADRGToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ MWSI/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MWSI
+{
+    public partial class Form1 : Form
+    {
+        Size ImageSize;
+        List<PictureBox> Maps = new List<PictureBox>();
+        public static float value = 1.0f;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Layer()
+        {
+            PictureBox pb = new PictureBox();
+            ImageSize = new Size(pb.Size.Height, pb.Size.Height);
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb.Size = flowLayoutPanel1.Size;
+            ImageSize = new Size(pb.Size.Height, pb.Size.Height);
+
+            OpenFileDialog ofp = new OpenFileDialog();
+            if (ofp.ShowDialog() == DialogResult.OK)
+            {
+                pb.Image = new Bitmap(ofp.FileName);
+                Maps.Add(pb);
+
+                flowLayoutPanel1.Controls.Add(pb);
+            }
+
+            
+
+            
+
+            this.MouseWheel += flowLayoutPanel1_MouseWheel;
+
+           
+
+
+        }
+
+
+        private void cADRGToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Layer();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Size = new Size(Width-180, Height-63);
+            Maps.ElementAt(0).Size = flowLayoutPanel1.Size;
+        }
+
+        private void flowLayoutPanel1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            PictureBox pb = Maps.ElementAt(0);
+            if(e.Delta > 0)
+            {
+                value+=0.1f;
+                pb.Size = new Size((int)(ImageSize.Height * value), (int)(ImageSize.Width * value));
+            }
+            else if (e.Delta < 0)
+            {
+                
+                if (value > 0)
+                {
+                    value-=0.1f;
+                    pb.Size = new Size((int)(ImageSize.Height * value), (int)(ImageSize.Width * value));
+
+                }
+                else value = 0;
+            }
+            
+        }
+
+    }
+}
